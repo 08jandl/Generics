a943666a85486e2a46403f55f04b0f1a12d755d3
08jandl <silke8421@gmx.at>
2025-04-01 15:59:50 +0200

    Everything we did April 1st, afternoon session

---
+++ src/hotel/Guest.java
@@ -0,0 +1,37 @@
+package hotel;
+
+public class Guest {
+    private Key key;
+    private int roomNumber;
+    private String name;
+
+    public Guest(Key key, int roomNumber, String name) {
+        this.key = key;
+        this.roomNumber = roomNumber;
+        this.name = name;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
